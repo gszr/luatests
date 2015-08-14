86f6cad1e3c4b91c9e7fefa98fa851cafc3924a5
Guilherme Salazar <gmesalazar@gmail.com>
2015-08-13 21:58:47 -0300

    use kernel random

--- modules/luamath/luamath.c
+++ modules/luamath/luamath.c
@@ -49,51 +49,10 @@ math_fmod(lua_State *L)
 	return 1;
 }
 
-/* RNG impl shamelessly copied from https://en.wikipedia.org/wiki/Multiply-with-carry */
-#define PHI 0x9e3779b9
-static uint32_t Q[4096], c = 362436;
-
-static void 
-init_rand(uint32_t x)
-{
-	int i;
-
-    Q[0] = x;
-	Q[1] = x + PHI;
-	Q[2] = x + PHI + PHI;
-
-	for (i = 3; i < 4096; i++)
-		Q[i] = Q[i - 3] ^ Q[i - 2] ^ PHI ^ i;
-}
-
-static uint32_t 
-rand_cmwc(void)
-{
-	uint64_t t, a = 18782LL;
-	static uint32_t i = 4095;
-	uint32_t x, r = 0xfffffffe;
-	i = (i + 1) & 4095;
-	t = a * Q[i] + c;
-	c = (t >> 32);
-	x = t + c;
-	if (x < c) {
-		x++;
-		c++;
-	}
-	return (Q[i] = r - x);
-}
-
-/* RND impl end */
-
 static int
 math_random(lua_State *L)
 {
-    struct bintime bt;
-	bintime(&bt);
-
-	init_rand(bt.sec);
-	lua_pushinteger(L, rand_cmwc());
-
+	lua_pushinteger(L, random());
 	return 1;
 }
 
