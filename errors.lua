146ce92bb0f44db47edbe96e2f8b18af4f4c9204
Guilherme Salazar <gmesalazar@gmail.com>
2015-08-18 15:16:36 -0300

    use io and math libs

--- errors.lua
+++ errors.lua
@@ -52,10 +52,7 @@ assert(doit("error()") == nil)
 
 -- test common errors/errors that crashed in the past
 assert(doit("table.unpack({}, 1, n=2^30)"))
--- XXX Kernel Lua: no std math lib
-if not _KERNEL then
 assert(doit("a=math.sin()"))
-end
 assert(not doit("tostring(1)") and doit("tostring()"))
 assert(doit"tonumber()")
 assert(doit"repeat until 1; a")
@@ -79,10 +76,7 @@ checkmessage("a = {} | 1", "bitwise operation")
 checkmessage("a = {} < 1", "attempt to compare")
 checkmessage("a = {} <= 1", "attempt to compare")
 
--- XXX Kernel Lua: no std math lib
-if not _KERNEL then
 checkmessage("a=1; bbbb=2; a=math.sin(3)+bbbb(3)", "global 'bbbb'")
-end
 checkmessage("a={}; do local a=1 end a:bbbb(3)", "method 'bbbb'")
 checkmessage("local a={}; a.bbbb(3)", "field 'bbbb'")
 assert(not string.find(doit"a={13}; local bbbb=1; a[bbbb](3)", "'bbbb'"))
@@ -111,11 +105,8 @@ checkmessage("aaa='2'; b=nil;x=aaa*b", "global 'b'")
 checkmessage("aaa={}; x=-aaa", "global 'aaa'")
 
 -- short circuit
--- XXX Kernel Lua: no std math lib
-if not _KERNEL then
 checkmessage("a=1; local a,bbbb=2,3; a = math.sin(1) and bbbb(3)",
        "local 'bbbb'")
-end
 checkmessage("a=1; local a,bbbb=2,3; a = bbbb(1) or a(3)", "local 'bbbb'")
 checkmessage("local a,b,c,f = 1,1,1; f((a and b) or c)", "local 'f'")
 checkmessage("local a,b,c = 1,1,1; ((a and b) or c)()", "call a number value")
@@ -159,23 +150,16 @@ checkmessage([[
 ]], "light userdata")
 _G.D = nil
 
--- XXX Kernel Lua TODO math.sin, io.input
-if not _KERNEL then
 do   -- named userdata
   checkmessage("math.sin(io.input())", "(number expected, got FILE*)")
   _ENV.XX = setmetatable({}, {__name = "My Type"})
 
-  -- TODO: io lib
   checkmessage("io.input(XX)", "(FILE* expected, got My Type)")
   _ENV.XX = nil
 end
-end
 
 -- global functions
--- TODO: io lib
-if not _KERNEL then
 checkmessage("(io.write or print){}", "io.write")
-end
 checkmessage("(collectgarbage or print){}", "collectgarbage")
 
 -- errors in functions without debug info
@@ -206,8 +190,6 @@ checkmessage("local _ENV=_ENV;"..s.."; a = bbb + 1", "global 'bbb'")
 checkmessage(s.."; local t = {}; a = t.bbb + 1", "field 'bbb'")
 checkmessage(s.."; local t = {}; t:bbb()", "method 'bbb'")
 
--- XXX Kernel Lua: TODO math lib
-if not _KERNEL then
 checkmessage([[aaa=9
 repeat until 3==3
 local x=math.sin(math.cos(3))
@@ -223,7 +205,6 @@ checkmessage([[
 local x,y = {},1
 if math.sin(1) == 0 then return 3 end    -- return
 x.a()]], "field 'a'")
-end
 
 checkmessage([[
 prefix = nil
@@ -234,12 +215,9 @@ while 1 do
   insert(prefix, a)
 end]], "global 'insert'")
 
--- XXX Kernel Lua: TODO math lib
-if not _KERNEL then
 checkmessage([[  -- tail call
   return math.sin("a")
 ]], "'sin'")
-end
 
 checkmessage([[collectgarbage("nooption")]], "invalid option")
 
@@ -247,10 +225,7 @@ checkmessage([[x = print .. "a"]], "concatenate")
 checkmessage([[x = "a" .. false]], "concatenate")
 checkmessage([[x = {} .. 2]], "concatenate")
 
--- XXX Kernel Lua: TODO io lib
-if not _KERNEL then
 checkmessage("getmetatable(io.stdin).__gc()", "no value")
-end
 
 checkmessage([[
 local Var
