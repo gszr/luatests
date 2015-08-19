1bf9bd6ebda62e4a8b83f571dba1453e992160a1
Guilherme Salazar <gmesalazar@gmail.com>
2015-08-19 02:32:12 -0300

    rename

---
+++ kernel/exec-tests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# Execute all test scripts using luactl
+
+if [ $# -eq 0 ]; then
+	echo "Usage $0 lua_state_name"
+	exit 1
+fi
+
+echo;
+sudo modload lua 
+sudo modload luaos 
+sudo modload luaio
+sudo modload luamath
+sudo sysctl -w kern.lua.verbose=1
+sudo sysctl -w ddb.onpanic=1
+sudo luactl -c openlibs $1
+
+echo;
+luactl load $1 ./preload.lua;
+
+for i in `find . -name "*.lua" -maxdepth 1`; do
+	case $i in
+		*all.lua|*big.lua|*files.lua|*math.lua|*main.lua|*preload.lua) ;;
+		*) luactl load $1 $i;
+	esac
+done
